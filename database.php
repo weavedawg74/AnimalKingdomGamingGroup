6b83f46303bbcb4165bdfb595efc1413b7807114
Andrew Weaverling <weavedawg74@yahoo.com>
2021-06-30 22:10:55 -0500

    Add files via upload

--- database.php
+++ database.php
@@ -1,10 +1,12 @@
 <?php
-$servername='localhost';
-$username='root';
-$password='';
-$dbname = "gamers";
-$conn=mysqli_connect($servername,$username,$password,"$dbname");
-if(!$conn){
-   die('Could not Connect My Sql:' .mysql_error());
-}
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
